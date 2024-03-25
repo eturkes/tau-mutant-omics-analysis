8ad60b90d1f651940c2f95ea844694759749e7f8
Emir Turkes <eturkes@bu.edu>
2024-03-25 18:28:15 +0000

    Refine prioritisation method

--- R/utils.R
+++ R/utils.R
@@ -969,7 +969,7 @@ word_cloud = function(x, width = NULL, dict = c()){
              "oligopeptide", "oligopeptides", "balance", "gshgssg",
              "indicating", "nadnadh", "cytoplasms", "cytoplasm",
              "cytoplasmic", "daltons", "dalton", "live",
-             "offspring", "intramitochondria", "intramitochondrial"),
+             "offspring", "intramitochondria", "intramitochondrial", "stretch"),
            dict,
            c("mitosis", "heme", "spermiogenesis", "prolactin",
              "trophoblast", "adipose", "hepatocyte", "follicles",
